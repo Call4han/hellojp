a02236f1443d36a9cc5a6c5263cd4bfc1e41f986
Call4han <ccasarider@gmail.com>
2024-04-10 15:41:28 -0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("java")
+    id("maven-publish")
+}
+
+group = "me.callahandev"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    maven("https://jitpack.io")
+    mavenCentral()
+}
+
+dependencies {
+
+}
